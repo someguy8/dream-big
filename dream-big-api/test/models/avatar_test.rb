b2b186963d9447d2f2c4461d1893814636ac5da2
jbeui <84165478+jbeui@users.noreply.github.com>
2022-12-06 22:33:55 +1100

    test: additional tests for avatar
    Altered fixtures files with file path attribute and added more tests to the avatar_test file.

--- dream-big-api/test/models/avatar_test.rb
+++ dream-big-api/test/models/avatar_test.rb
@@ -27,4 +27,32 @@ class AvatarTest < ActiveSupport::TestCase
     assert_equal avatar_accessories(:accessory).id, avatar(:avatar).avatar_accessories_id
   end
 
+  test "should modify avatar accessories id and save avatar record " do
+    avatar = avatar(:avatar)
+    avatar.accessories_id = avatar_accesories(:accessory_2).id
+    avatar.save
+    assert_equal( avatar_hair(:accessory_2).id, avatar.accessories_id, "accessories_id not modified")
+    end 
+    
+  test "should modify avatar hair id and save avatar record " do
+    avatar = avatar(:avatar)
+    avatar.hair_id = avatar_hair(:hair_2).id
+    avatar.save
+    assert_equal( avatar_hair(:hair_2).id, avatar.hair_id, "hair_id not modified")
+  end
+
+  test "should modify avatar head id and save avatar record " do
+    avatar = avatar(:avatar)
+    avatar.head_id = avatar_head(:head_2).id
+    avatar.save
+    assert_equal( avatar_hair(:head_2).id, avatar.head_id, "head_id not modified")
+  end
+
+  test "should modify avatar torso id and save avatar record " do
+    avatar = avatar(:avatar)
+    avatar.torso_id = avatar_torso(:torso_2).id
+    avatar.save
+    assert_equal( avatar_hair(:torso_2).id, avatar.torso_id, "torso_id not modified")
+  end
+
 end
